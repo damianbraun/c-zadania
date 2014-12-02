8eabdb796ae8995ef631454d268bf2c2391ecf43
Damian Braun <brunek5252@gmail.com>
2014-12-02 11:37:08 +0100

    zadanie 3

---
+++ 7-funkcje/z3.c
@@ -0,0 +1,29 @@
+/* Liczba doskonała to liczba, która jest sumą podzielników od niej mniejszych. Na przykład 6 jest liczbą doskonałą ponieważ:
+
+1 + 2 + 3 = 6
+
+Napisz funkcję, która sprawdza czy podana liczba jest doskonała. Użyj tej funkcji do wypisania wszystkich liczb doskonałych mniejszych od 10 000.*/
+#include <stdio.h>
+
+int isPerfectNumber(int number) {
+  int wynik = 0;
+  for (int i = 1; i < number; i++) {
+    if (number % i == 0) {
+      wynik = wynik + i;
+    }
+  }
+  if (wynik == number) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+int main() {
+  for (int i = 1; i < 10000; i++) {
+    if (isPerfectNumber(i) == 1) {
+      printf("%i\tjest doskonala\n", i);
+    }
+  }
+}
